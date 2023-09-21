4557c865a9b093dada0386299ec60b6ee1b36ff4
Anderson Bispo <anderson.bispo@4linux.com.br>
2023-09-20 21:24:21 -0300

    vpc

--- Aula02/codes/vpc.tf
+++ Aula02/codes/vpc.tf
@@ -1,5 +1,6 @@
 resource "google_compute_network" "vpc_terraform_lab" {
   name = "vpc-terraform-lab"
-  project = "projeto-teste"
+  #project = "projeto-teste"
+  project = "labs-terraform-8476"
   auto_create_subnetworks = false
 }
