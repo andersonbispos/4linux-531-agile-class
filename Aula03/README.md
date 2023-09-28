527f6131e9c4cae5ae0989ca1350caa2aecde01c
Anderson Bispo <anderson.bispo@4linux.com.br>
2023-09-27 21:20:33 -0300

    aula03

--- Aula03/README.md
+++ Aula03/README.md
@@ -4,7 +4,14 @@
 
 ## Referências
 
+### impersonation
+
 https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/getting_started#configuring-the-provider
 https://medium.com/google-cloud/a-hitchhikers-guide-to-gcp-service-account-impersonation-in-terraform-af98853ebd37
-https://cloud.google.com/blog/topics/developers-practitioners/using-google-cloud-service-account-impersonation-your-terraform-code
+https://cloud.google.com/blog/topics/developers-practitioners/
+
+### import block
+
+using-google-cloud-service-account-impersonation-your-terraform-code
+https://cloud.google.com/docs/terraform/resource-management/import
 https://developer.hashicorp.com/terraform/tutorials/state/state-import
--- Aula03/codes/provider.tf
+++ Aula03/codes/provider.tf
@@ -7,6 +7,23 @@ terraform {
   }
 }
 
-provider "google" { 
-  project = "labs-terraform-8476"
+provider "google" {
+ alias = "impersonation"
+ scopes = [
+   "https://www.googleapis.com/auth/cloud-platform",
+   "https://www.googleapis.com/auth/userinfo.email",
+ ]
+}
+
+data "google_service_account_access_token" "default" {
+ provider               	= google.impersonation
+ target_service_account 	= "terraform-iac@terraform-8476-aulas.iam.gserviceaccount.com"
+ scopes                 	= ["userinfo-email", "cloud-platform"]
+ lifetime               	= "300s"
+}
+
+provider "google" {
+ project 		= "terraform-8476-aulas"
+ access_token	= data.google_service_account_access_token.default.access_token
+ request_timeout 	= "60s"
 }
