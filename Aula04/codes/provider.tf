398902007539995e70f4bbc2d1bb0a92fafbab80
Anderson Bispo <anderson.bispo@4linux.com.br>
2023-10-04 20:52:08 -0300

    aula 04 files

--- Aula04/codes/provider.tf
+++ Aula04/codes/provider.tf
@@ -1,12 +1,18 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "4.84.0"
     }
   }
+
+  backend "gcs" {
+    bucket = "trfm-8476-aulas-state"
+    prefix = "terraform/state-prod"
+  }
+
 }
 
 provider "google" {
- project 		= "terraform-8476-aulas"
+  project = "terraform-8476-aulas"
 }
--- Aula04/codes/subnets.tf
+++ Aula04/codes/subnets.tf
@@ -1,20 +1,20 @@
 
 resource "google_compute_subnetwork" "subnet1" {
-  ip_cidr_range              = "10.10.10.0/24"
+  ip_cidr_range = "10.10.10.0/24"
 
-  name                       = "subnet1"
-  network                    = google_compute_network.vpc_terraform_lab.self_link
+  name    = "subnet1"
+  network = google_compute_network.vpc_terraform_lab.self_link
 
-  region                     = "southamerica-east1"
+  region = "southamerica-east1"
 }
 
 resource "google_compute_subnetwork" "subnet2" {
 
-  ip_cidr_range              = "10.10.20.0/24"
+  ip_cidr_range = "10.10.20.0/24"
 
-  name                       = "subnet2"
-  network                    = google_compute_network.vpc_terraform_lab.self_link
+  name    = "subnet2"
+  network = google_compute_network.vpc_terraform_lab.self_link
 
-  region                     = "us-central1"
+  region = "us-central1"
 
 }
